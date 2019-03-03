d4922c7151b7f66ff6f6f9edd5d091119da9a3cd
Ryan Knight <ryanknight@eagles.ewu.edu>
2019-03-03 12:41:23 -0800

    init

---
+++ GUI_FileSysWatcher/GUI_FileSysWatcher/DatabaseWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Data.SQLite;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static GUI_FileSysWatcher.MainWindow;
+
+namespace GUI_FileSysWatcher
+{
+    /// <summary>
+    /// Interaction logic for DatabaseWindow.xaml
+    /// </summary>
+    public partial class DatabaseWindow : Window
+    {
+        private string _extension = ".";
+        private ObservableCollection<FileData> theListDB = new ObservableCollection<FileData>();
+        FileData dataDb;
+        SQLiteConnection sqlite_conn;
+        SQLiteCommand sqlite_cmd;
+        SQLiteDataReader sqlite_datareader;
+        public DatabaseWindow()
+        {
+            InitializeComponent();
+            this.cleardb.IsEnabled = true;
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.gridBoxDB.Items.Count == 0)
+            {
+                QueryDB();
+            } else
+            {
+                theListDB.Clear();
+                this.gridBoxDB.ItemsSource = null;
+                this.gridBoxDB.Items.Refresh();
+                QueryDB();
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem typeItem = (ComboBoxItem)comboBox.SelectedItem;
+
+            if (typeItem.Content.ToString().EndsWith("Any file type"))
+            {
+                this._extension = ".";
+            }
+            else
+            {
+                this._extension = typeItem.Content.ToString();
+            }
+        }
+
+        private void extBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this._extension = "." + this.extBox.Text;
+        }
+
+        private void QueryDB()
+        {
+            this.sqlite_conn = new SQLiteConnection("Data Source=database.db");
+            this.sqlite_conn.Open();
+            sqlite_cmd = sqlite_conn.CreateCommand();
+
+            if (this._extension.Equals("."))
+            {
+                sqlite_cmd.CommandText = "SELECT * FROM watcherLog";
+            }
+            else
+            {
+                sqlite_cmd.CommandText = "SELECT * FROM watcherLog WHERE Ext = " + "'" + this._extension + "'";
+            }
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+
+            while (sqlite_datareader.Read())
+            {
+                this.dataDb = new FileData(sqlite_datareader["FileName"].ToString(), sqlite_datareader["absPath"].ToString(),
+                                           sqlite_datareader["Event"].ToString(), sqlite_datareader["Date"].ToString(),
+                                           sqlite_datareader["Ext"].ToString());
+
+                this.theListDB.Add(this.dataDb);
+            }
+            sqlite_conn.Close();
+
+            this.gridBoxDB.ItemsSource = this.theListDB;
+        }
+
+        private void clearDB_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to clear the Database? It cannot be undone!!!", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                this.sqlite_conn = new SQLiteConnection("Data Source=database.db");
+                this.sqlite_conn.Open();
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "DELETE FROM watcherlog";
+                sqlite_datareader = sqlite_cmd.ExecuteReader();
+                sqlite_conn.Close();
+                this.cleardb.IsEnabled = false;
+                this.showCleardb.Content = "Database has been Cleared!";
+            }
+                
+        }
+    }
+}
---
+++ GUI_FileSysWatcher/GUI_FileSysWatcher/MainWindow.xaml.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Security.Permissions;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_FileSysWatcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<FileData> theList = new ObservableCollection<FileData>();
+        private String extention;
+        private String dir;
+        private FileSystemWatcher Watcher;
+        SQLiteConnection sqlite_conn;
+        SQLiteCommand sqlite_cmd;
+
+        public ObservableCollection<FileData> List
+        {
+            get
+            {
+                return this.theList;
+            }
+        }
+
+        public class FileData
+        {
+            private string _fileName;
+            private string _absPath;
+            private string _eventType;
+            private string _date;
+            private string _ext;
+
+            public FileData(string fileName, string absPath, string eventType, string date, string ext)
+            {
+                this._fileName = fileName;
+                this._absPath = absPath;
+                this._eventType = eventType;
+                this._date = date;
+                this._ext = ext;
+            }
+
+            public string FileName
+            {
+                get
+                {
+                    return this._fileName;
+                }
+                set
+                {
+                    this._fileName = value;
+                }
+            }
+
+            public string Extenion
+            {
+                get
+                {
+                    return this._ext;
+                }
+                set
+                {
+                    this._ext = value;
+                }
+            }
+
+            public string EventType
+            {
+                get
+                {
+                    return this._eventType;
+                }
+                set
+                {
+                    this._eventType = value;
+                }
+            }
+
+            public string Date
+            {
+                get
+                {
+                    return this._date;
+                }
+                set
+                {
+                    this._date = value;
+                }
+            }
+
+            public string absPath
+            {
+                get
+                {
+                    return this._absPath;
+                }
+                set
+                {
+                    this._absPath = value;
+                }
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupDB();
+        }
+
+        private void SetupWatcher()
+        {
+            this.gridBox.ItemsSource = theList;
+            Watcher = new FileSystemWatcher(@dir);
+                Watcher.IncludeSubdirectories = true;
+            Watcher.Filter = "*" + this.extention + "*";
+
+                Watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName |
+                                        NotifyFilters.DirectoryName;
+           
+                Watcher.Changed += new FileSystemEventHandler(OnChanged);
+                Watcher.Created += new FileSystemEventHandler(OnChanged);
+                Watcher.Deleted += new FileSystemEventHandler(OnChanged);
+                //Watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+                Watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(Object source, FileSystemEventArgs e)
+        {
+            
+                Dispatcher.BeginInvoke(
+                  (Action)(() =>
+                  {
+                      FileData myData = new FileData(System.IO.Path.GetFileName(e.Name), e.FullPath, e.ChangeType.ToString(), DateTime.Now.ToLongDateString(), System.IO.Path.GetExtension(e.FullPath));
+                      theList.Add(myData);
+                      
+                  }));
+            
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            this.showDir.Content = "Directory: " + this.dir;
+            SetupWatcher();
+            this.Querydb.IsEnabled = false;
+            this.Start.IsEnabled = false;
+            this.Stop.IsEnabled = true;
+        }
+
+        private void CustomBoxExt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.extention = "." + this.customExt.Text;
+        }
+
+        private void directoryIn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.dir = this.directoryIn.Text;
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            this.Watcher.Dispose();
+            this.showDir.Content = "******* Watcher has stopped *******";
+            this.Stop.IsEnabled = false;
+            this.Start.IsEnabled = true;
+            this.Querydb.IsEnabled = true;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Copywrite 2018 Ryan Knight .Net FrameWork 64 bit. This" +
+                            "program will watch a given directory for events that happen within it. " +
+                            "To begin, select a file extension from the drop down menu or type your own into " +
+                            "the text field. Next enter your directory that you wish to watch then click start. " +
+                            "You can click stop at anytime and start again after at anytime. " +
+                            "If you choose to add the current log to a database the log will clear but you can click " +
+                            "query to search for past log files. ");
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Version 1.0, and probly the last...  :)");
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Made by Ryan Knight");
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem typeItem = (ComboBoxItem)comboBox.SelectedItem;
+
+            if(typeItem.Content.ToString().EndsWith("Any file type"))
+            {
+                this.extention = ".";
+            } else
+            {
+                this.extention = typeItem.Content.ToString();
+            }
+            
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to clear this window? Data will be lost without writting to database first!", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                theList.Clear();
+                this.gridBox.ItemsSource = null;
+                this.gridBox.Items.Refresh();
+            }
+            
+        }
+
+        private void SetupDB()
+        {
+            if (File.Exists("database.db"))
+            {
+                this.sqlite_conn = new SQLiteConnection("Data Source=database.db");
+                this.sqlite_conn.Open();
+                this.sqlite_cmd = sqlite_conn.CreateCommand();
+
+                foreach(FileData d in theList)
+                {
+                    this.sqlite_cmd.CommandText = "INSERT INTO watcherLog (FileName, absPath, Event, Date, Ext) VALUES ('"+ d.FileName + "', '" + d.absPath + "', '" + d.EventType + "', '" + d.Date + "', '" + d.Extenion + "');";
+                    this.sqlite_cmd.ExecuteNonQuery();
+                }
+
+                this.sqlite_conn.Close();
+                
+            } else
+            {
+                SQLiteConnection.CreateFile("database.db");
+                this.sqlite_conn = new SQLiteConnection("Data Source=database.db");
+                this.sqlite_conn.Open();
+                this.sqlite_cmd = sqlite_conn.CreateCommand();
+                this.sqlite_cmd.CommandText = "CREATE TABLE watcherLog (FileName varchar(50), absPath varchar(100), Event varchar(25), Date varchar(25), Ext varchar(10));";
+                this.sqlite_cmd.ExecuteNonQuery();
+
+                foreach (FileData d in theList)
+                {
+                    this.sqlite_cmd.CommandText = "INSERT INTO watcherLog (FileName, absPath, Event, Date, Ext) VALUES ('" + d.FileName + "', '" + d.absPath + "', '" + d.EventType + "', '" + d.Date + "', '" + d.Extenion + "');";
+                    this.sqlite_cmd.ExecuteNonQuery();
+                }
+
+                this.sqlite_conn.Close();
+            }
+            
+        }
+
+        private void ToDB_Click(object sender, RoutedEventArgs e)
+        {
+            Window Dbw = new DatabaseWindow();
+            Dbw.ShowDialog();
+        }
+
+        private void addDB_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Current log will clear after writting to database. Continue?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                SetupDB();
+                this.theList.Clear();
+                this.gridBox.ItemsSource = null;
+                this.gridBox.Items.Refresh();
+                this.showDir.Content = "Logs written to database succesfully!";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Write current log to database before close?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                SetupDB();
+                theList.Clear();
+                this.gridBox.ItemsSource = null;
+                this.gridBox.Items.Refresh();
+                Environment.Exit(0);
+            } else
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
